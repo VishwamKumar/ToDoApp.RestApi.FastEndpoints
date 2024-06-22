0560d39ef2abda2d8017a02480def08d835106c1
Vishwam <vishwamkumar@outlook.com>
2024-06-22 12:12:16 -0400

    Initial commit

---
+++ GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using ToDoApp.DataLibrary.Sqlite.Models;
+global using ToDoApp.DataLibrary.Sqlite.Services;
+global using ToDoApp.RestApi.FastEndpoints.Dtos;
+global using System.ComponentModel.DataAnnotations;
+global using FastEndpoints.Swagger;
+global using FastEndpoints;
+global using AutoMapper;
+
